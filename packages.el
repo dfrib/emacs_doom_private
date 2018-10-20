bcf946a424e48d676432ee0d7bdd4364db1c5340
dfrib <david.n.friberg@gmail.com>
2018-10-20 20:56:13 +0200

    Package.el: remove unused packages

--- packages.el
+++ packages.el
@@ -4,16 +4,7 @@
 (package! lsp-ui)
 (package! ccls)
 (package! cquery)
-;(package! org-noter)
-;(package! esh-autosuggest)
-;(package! fish-completion)
-;(package! docker-tramp)
-;(package! org-super-agenda)
-;(package! org-brain)
-
-(package! goto-last-change)
 (package! resize-window)
-(package! www-synonyms)
 (package! ag)
 
 ;(package! ob-ipython :recipe (:fetcher github :repo "fuxialexander/ob-ipython" :files ("*.el" "*.py")))
